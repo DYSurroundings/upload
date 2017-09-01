799d215ccc9b2b03478ccc129c6e1ed952563437
michaeljpitz <michaeljpitz@gmail.com>
2017-09-01 16:09:07 +0200

    création vue

--- index.php
+++ index.php
@@ -5,5 +5,13 @@
  */
 
 
-// chargement des bibliothèques externes (chargées par composer)
+// chargement des bibliothèques externes (chargées par composer) pour twig
 require_once 'vendor/autoload.php';
+
+// création du loader twig
+$loader = new Twig_Loader_Filesystem('v');
+$twig = new Twig_Environment($loader, array(
+    'cache' => 'cache',
+));
+
+echo $twig->render('base.html.twig');
